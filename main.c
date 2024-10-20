3f8b414877a632db59cdc7ebca20d262ff74e762
lucien MAC <lucien.dacunha@gmail.com>
2024-10-20 14:04:45 +0200

    OK

--- main.c
+++ main.c
@@ -6,12 +6,12 @@
 /*   By: luciendacunha <luciendacunha@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 11:35:36 by luciendacun       #+#    #+#             */
-/*   Updated: 2024/10/20 13:56:32 by luciendacun      ###   ########.fr       */
+/*   Updated: 2024/10/20 14:04:05 by luciendacun      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	<unistd.h>
-#include	<stdlib.h>
+#include	<stdio.h>
 
 void ft_putchar(char c)
 {
@@ -22,14 +22,16 @@ int	ft_strlen(char* str)
 {
 	int i;
 	i = 0;
-	if(str[i] != 0)
+	while(str[i])
+	{
 		i++;
-	retrun(i);
+	}
+	return(i);
 }
 
 int main(int argc, char **argv)
 {
-	char str[] = ""
-	printf('');
+	char *str = "tata";
+	printf("%d", ft_strlen(str));
 	return 0;	
 }
